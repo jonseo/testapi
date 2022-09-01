ec7361a3ebf974bc673edab0639ff2511a36b955
Jon Ekse <jrekse@gmail.com>
2022-09-01 11:28:43 -0700

    added another set of data, added corresponding endpoint

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 var fs = require('fs');
 
-// json file with the data
-var data = fs.readFileSync('poke2.json');
+// Establish data
+var pokeData = fs.readFileSync('poke2.json');
+var poke = JSON.parse(pokeData);
+var colorData = fs.readFileSync('colors.json');
+var color = JSON.parse(colorData)
 
-var poke = JSON.parse(data);
+// deal with dependencies etc
 const express = require("express");
 const app = express();
-  
-// To solve the cors issue
 const cors=require('cors');
-   
 app.listen(process.env.PORT, 
-    () => console.log("Server Start at the Port"));
-   
+
+  () => console.log("Server Start at the Port")
+
+);
 app.use(express.static('public'));
 app.use(cors());
 
-// when get request is made, alldata() is called
-app.get('/poke', alldata);
-  
-function alldata(request, response) {
+// Poke request and serve
+app.get('/poke', pokeDataf);
+function pokeDataf(request, response) {
       
-    // Returns all information about the elements
-    response.send(poke);
+  response.send(poke);
+
 }
 
+// Color request and serve
+app.get('/colors', colorDataf);
+function colorDataf(request, response) {
+
+  response.send(color)
+
+}
