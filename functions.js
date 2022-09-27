708d827fd3d46b743f73afcbb20747a0d35affe2
Jon Ekse <jrekse@gmail.com>
2022-09-27 11:06:04 -0700

    added ammo data

---
+++ functions.js
@@ -0,0 +1,149 @@
+var all = document.getElementById('allList')
+
+var poke = document.getElementById('pokeList')
+var pokeSelect = document.getElementById('pokeRadio')
+
+var color = document.getElementById('colorList')
+var colorSelect = document.getElementById('colorRadio')
+
+let allData = []
+
+let pokeData = []
+
+let colorData = []
+
+fetch('https://eksmanapi.herokuapp.com/all').then(function (response) {
+
+	return response.json();
+
+}).then(function (data) {
+
+    data.forEach((item) => {
+
+        item.forEach((itemm) => {
+
+            allData.push(itemm.title || itemm.color)
+
+        })
+        
+    })
+
+}).catch(function (err) {
+	
+	console.warn('Something went wrong.', err);
+
+});
+
+fetch('https://eksmanapi.herokuapp.com/poke').then(function (response) {
+
+	return response.json();
+
+}).then(function (data) {
+
+    data.forEach((item) => {
+
+        pokeData.push(item.title)
+
+    })
+
+}).catch(function (err) {
+	
+	console.warn('Something went wrong.', err);
+
+});
+
+fetch('https://eksmanapi.herokuapp.com/colors').then(function (response) {
+	
+	return response.json();
+
+}).then(function (data) {
+	
+    data.forEach((item) => {
+
+        colorData.push(item.color)
+
+    })
+
+    display()
+
+}).catch(function (err) {
+
+	console.warn('Something went wrong.', err);
+
+});
+
+function display(){
+
+    pokeData.forEach((item) => {
+
+        let li = document.createElement("li");
+
+        li.innerText = item;
+
+        poke.appendChild(li);
+
+    });
+    colorData.forEach((item) => {
+
+        let li = document.createElement("li");
+
+        li.innerText = item;
+
+        color.appendChild(li);
+
+    });
+    allData.forEach((item) => {
+
+        let li = document.createElement("li");
+
+        li.innerText = item;
+
+        all.appendChild(li);
+
+    });
+
+}
+
+function addDataSelector() {
+
+    if (colorSelect === true){
+        addColorData()
+    } else if (pokeSelect === true){
+        addPokeData()
+    }
+
+}
+
+function addColorData(){
+
+    console.log('color')
+
+    // fs.readFile('colors.json', function (err, data) {
+
+    //     var json = JSON.parse(data)
+        
+    //     json.push('color : ' + currentSearchResult)
+
+    //     json.push('value : ' + currentSearchResult)
+    
+    //     fs.writeFile("colors.json", JSON.stringify(json))
+    // })
+        
+}
+
+function addPokeData(){
+
+    console.log('poke')
+
+    // fs.readFile('poke.json', function (err, data) {
+
+    //     var json = JSON.parse(data)
+
+    //     json.push('title : ' + currentSearchResult)
+
+    //     json.push('img : ' + currentSearchResult)
+    
+    //     fs.writeFile("poke2.json", JSON.stringify(json))
+    // })
+        
+}
--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 var fs = require('fs');
 
 // Establish data
-var pokeData = fs.readFileSync('poke2.json');
-var poke = JSON.parse(pokeData);
-var colorData = fs.readFileSync('colors.json');
-var color = JSON.parse(colorData)
+var pokeDataj = fs.readFileSync('poke2.json');
+var poke = JSON.parse(pokeDataj);
+
+var colorDataj = fs.readFileSync('colors.json');
+var color = JSON.parse(colorDataj)
+
+var ammoDataj = fs.readFileSync('ammo.json');
+var ammo = JSON.parse(ammoDataj)
 
 // deal with dependencies etc
 const express = require("express");
 const app = express();
+app.use(express.json());
 const cors=require('cors');
-const { type } = require('os');
 app.listen(process.env.PORT, 
-
   () => console.log("Server Start at the Port")
-
 );
 app.use(express.static('public'));
 app.use(cors());
 
-console.log(typeof {color})
-console.log(typeof {poke})
+// Poke post
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Thing created successfully!'
+  });
+});
 
 // Poke request and serve
 app.get('/poke', pokeDataf);
 function pokeDataf(request, response) {
-      
   response.send(poke);
-
 }
 
-// Color request and serve
 app.get('/colors', colorDataf);
 function colorDataf(request, response) {
-
   response.send(color)
+}
 
+app.get('/ammo', ammoDataf);
+function ammoDataf(request, response) {
+  response.send(ammo)
 }
 
 app.get('/all', allDataf);
 function allDataf(request, response) {
-
   response.send([color, poke])     
-
 }
